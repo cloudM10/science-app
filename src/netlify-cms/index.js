606382667a70e1324670d165ca05d274222507d2
Vadim Ivchenko <47832273+vdmemory@users.noreply.github.com>
2025-10-21 11:04:33 +0300

    init new

--- src/netlify-cms/index.js
+++ src/netlify-cms/index.js
@@ -4,18 +4,9 @@ import registerPdfLinkWidget from "./pdf-link-widget";
 
 CMS.init({
     config: {
-        load_config_file: true, // загружает config.yml автоматически
-        config_file_path: "/admin/config.yml", // путь к файлу на сайте
         backend: {
-            name: "github",
-            repo: "cloudM10/science-app",
-            branch: "main",
-            auth_type: "implicit",
-            base_url: "https://orca-app-xfbmd.ondigitalocean.app",
-            auth_endpoint: "/auth",
+            name: "git-gateway",
         },
-        media_folder: "static/uploads",
-        public_folder: "/uploads",
     },
 });
 
