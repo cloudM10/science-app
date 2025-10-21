3dbe554d0a1a4e5a6a8977efdfd0810019225d84
Vadim Ivchenko <47832273+vdmemory@users.noreply.github.com>
2025-10-21 04:36:01 +0300

    update config

--- src/netlify-cms/index.js
+++ src/netlify-cms/index.js
@@ -2,7 +2,21 @@ import CMS from "netlify-cms-app";
 import registerTableWidget from "./table-widget";
 import registerPdfLinkWidget from "./pdf-link-widget";
 
-CMS.init();
+CMS.init({
+    config: {
+        backend: {
+            name: "github",
+            repo: "cloudM10/science-app",
+            branch: "main",
+            auth_type: "implicit",
+            app_id: "Ov23lien0vEnJZOumzEn",
+            base_url: "https://orca-app-xfbmd.ondigitalocean.app",
+            auth_endpoint: "/auth",
+        },
+        media_folder: "static/uploads",
+        public_folder: "/uploads",
+    },
+});
 
 registerTableWidget(CMS);
 registerPdfLinkWidget(CMS);
